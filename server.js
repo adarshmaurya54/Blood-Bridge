71af20194b65105e5cfa11ba58159039f9516e8e
Adarsh Maurya <adarshmaurya0022@gmail.com>
2024-10-16 21:58:37 +0530

    path, static folder and static routes

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const colors = require('colors')
 const morgan  = require('morgan')
 const cors = require('cors');
 const connectDB = require('./config/db');
+const path = require("path")
 
 //dot env config
 dotenv.config();
@@ -27,6 +28,14 @@ app.use('/api/v1/inventory', require('./routes/inventoryRoutes'));
 app.use('/api/v1/analytics', require('./routes/analyticsRoutes'));
 app.use('/api/v1/admin', require('./routes/adminRoutes'));
 
+
+//static folder
+app.use(express.static(path.join(__dirname,'./client/dist')))
+
+// static routes
+app.get("*", function (req, res){
+    res.sendFile(path.join(__dirname, "./client/dist/index.html"))
+})
 // port
 const PORT = process.env.PORT || 8080;
 
