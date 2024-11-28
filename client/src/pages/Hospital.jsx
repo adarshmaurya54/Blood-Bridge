a3430cd478c3bd5233e2e72dffe653b240a49182
Adarsh Maurya <adarshmaurya0022@gmail.com>
2024-11-28 12:31:42 +0530

    hospital page comment added

--- client/src/pages/Hospital.jsx
+++ client/src/pages/Hospital.jsx
@@ -1,21 +1,21 @@
-import React, { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
-import API from "../services/API";
-import Spinner from "../components/shared/Spinner";
-import moment from "moment";
+import React, { useEffect, useState } from "react";// useEffect : handle side effects like API calls & useState : manage local state
+import { useSelector } from "react-redux";// useSelector : Fetches data from the Redux store.
+import API from "../services/API";//API: Manages backend calls.
+import Spinner from "../components/shared/Spinner";//Spinner: A loading spinner component.
+import moment from "moment";//moment: A library for formatting dates.
 import { FaHospitalAlt } from "react-icons/fa";
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";//useNavigate: Used for navigation between pages in the React Router.
 
-const Hospital = () => {
-  const { loading, user } = useSelector((state) => state.auth);
-  const [data, setData] = useState([]);
-  const navigate = useNavigate();
+const Hospital = () => { //Hospital Component
+  const { loading, user } = useSelector((state) => state.auth);//loading:Indicates if data is still loading & user:current logged-in user’s details.
+  const [data, setData] = useState([]);//data: Stores the list of donors fetched from the backend.
+  const navigate = useNavigate();//navigate: Used to redirect users to another page.
   if (user?.role === "donor") navigate("/");
   if (user?.role === "hospital") navigate("/");
   // find donor records
-  const getHospitals = async () => {
+  const getHospitals = async () => {  //asynchronous arrow function
     try {
-      const { data } = await API.get("/inventory/get-hospitals");
+      const { data } = await API.get("/inventory/get-hospitals");//sends a GET request to the server, await:function waits for the promise to resolve or reject 
       if (data?.success) {
         setData(data?.hospitals);
       }
@@ -23,7 +23,7 @@ const Hospital = () => {
       console.log(error);
     }
   };
-  useEffect(() => {
+  useEffect(() => { //any changes we want to do getHospitals() calls 
     getHospitals();
   }, []);
   return (
@@ -74,7 +74,7 @@ const Hospital = () => {
                     </tr>
                   </thead>
                   <tbody className="divide-y divide-black/25 dark:divide-neutral-700">
-                    {data?.map((record) => (
+                    {data?.map((record) => ( //maps all the arrays data rowise
                       <tr key={record._id}>
                         <td className="px-6 py-4 capitalize whitespace-nowrap text-sm font-medium text-gray-800 dark:text-gray-200">
                           {record.hospitalName ||
