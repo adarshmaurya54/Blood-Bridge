f083e46126c92df5587d472dfaa18e18be730b5b
Adarsh Maurya <adarshmaurya0022@gmail.com>
2024-12-01 11:27:25 +0530

    Seperated frontend and backend

---
+++ Backend/server.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const colors = require("colors");
+const morgan = require("morgan");
+const cors = require("cors");
+const connectDB = require("./config/db");
+const path = require("path");
+
+// Dotenv config
+dotenv.config();
+
+// MongoDB connection
+connectDB();
+
+// Create Express app
+const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+
+// Conditional logging for production
+if (process.env.NODE_ENV === "production") {
+  app.use(morgan("combined")); // Production logging
+} else {
+  app.use(morgan("dev")); // Development logging
+}
+
+// Routes
+app.use("/api/v1/auth", require("./routes/authRoutes"));
+app.use("/api/v1/inventory", require("./routes/inventoryRoutes"));
+app.use("/api/v1/analytics", require("./routes/analyticsRoutes"));
+app.use("/api/v1/admin", require("./routes/adminRoutes"));
+
+// Serve static files in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "frontend", "dist")));
+
+  // Handle all other routes by sending the frontend's index.html
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+  });
+} else {
+  console.log("Running in development mode.");
+}
+
+// Port Configuration
+const PORT = process.env.PORT || 8080;
+
+// Start server
+app.listen(PORT, () => {
+  console.log(
+    `Node server running in ${process.env.NODE_ENV} mode on port: ${PORT}`.bgBlue.white
+  );
+});
